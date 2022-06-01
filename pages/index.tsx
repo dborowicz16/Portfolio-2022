71e0d017b377c796aa2c213508d21ef44ed71562
dborowicz16 <79418706+dborowicz16@users.noreply.github.com>
2022-06-01 11:26:35 -0400

    added setting to fix type error

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,6 +88,7 @@ export default function Home() {
                   },
                   "enable": true,
                   "outModes": {
+                    "default": "out",
                     "bottom": "out",
                     "left": "out",
                     "right": "out",
